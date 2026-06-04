89fcb2da4f64e699fd828d94efe65445daf8b427
bukutori <cww97210@gmail.com>
2026-06-04 22:31:17 +0800

    連結

--- my-art-tools/vite.config.js
+++ my-art-tools/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/oyakimatu/', // 👈 記得改成你的 GitHub 專案名稱，前後都要有斜線！
 })
